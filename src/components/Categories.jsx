bcb8a91b0e41e5d40b8b88d4bf0e4be5af113718
MoriamAkterSwarna <moriam15-12880@diu.edu.bd>
2023-04-10 00:43:15 +0600

    job category added

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import SingleCategory from './SingleCategory';
 
 const Categories = () => {
     const categories = useLoaderData()
@@ -8,9 +9,11 @@ const Categories = () => {
         <div className='text-center'>
             <h2 className='text-4xl mb-5 font-bold '>Job Category List</h2>
             <p className='mb-10'>Choose Your Own Adventure: Explore Countless Career Possibilities with Our Curated Job Categories and Job Matching Technology</p>
+            <div className='grid sm:grid-cols-1 lg:grid-cols-4 md:grid-cols-2 gap-6'>
             {
-                categories.map(category => <p>{category.categoryName}</p>)
+                categories.map(category => <SingleCategory key={category.id} category={category}></SingleCategory>)
             }
+            </div>
         </div>
     );
 };
