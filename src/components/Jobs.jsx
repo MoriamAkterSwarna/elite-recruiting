98bc88d96867f35f35b72cf1c13fce934ef47cda
MoriamAkterSwarna <moriam15-12880@diu.edu.bd>
2023-04-10 01:42:10 +0600

    category image updated

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -5,7 +5,7 @@ const Jobs = () => {
         <div className='text-center my-6'>
             <h2 className='text-4xl font-bold mb-6'>Featured Jobs</h2>
             <p className='mb-10'>Exploring the World and Beyond: Job Opportunities for Travel and Adventure
-</p>
+                </p>
         </div>
     );
 };
